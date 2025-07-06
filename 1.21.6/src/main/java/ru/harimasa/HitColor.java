7cf9075e998c0b5f7d6b5bbb4b192e7134dad66e
harimasa <144851980+harimasa@users.noreply.github.com>
2025-07-06 22:54:53 +0200

    Add files via upload

---
+++ 1.21.6/src/main/java/ru/harimasa/HitColor.java
@@ -0,0 +1,17 @@
+package ru.harimasa;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import ru.harimasa.config.HCScreen;
+import ru.harimasa.listener.OverlayReloadListener;
+
+public class HitColor implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		HCScreen.CONFIG.load();
+		ClientTickEvents.END_WORLD_TICK.register((client) -> {
+			OverlayReloadListener.callEvent();
+		});
+	}
+}
