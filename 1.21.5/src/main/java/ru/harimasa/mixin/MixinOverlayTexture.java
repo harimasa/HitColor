6713d90f94d3181841c76439709adc3a8ca57439
harimasa <144851980+harimasa@users.noreply.github.com>
2025-07-09 19:28:47 +0200

    fix

--- 1.21.5/src/main/java/ru/harimasa/mixin/MixinOverlayTexture.java
+++ 1.21.5/src/main/java/ru/harimasa/mixin/MixinOverlayTexture.java
@@ -1,14 +1,13 @@
 package ru.harimasa.mixin;
 
 import com.mojang.blaze3d.opengl.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.render.OverlayTexture;
-import net.minecraft.client.texture.GlTexture;
 import net.minecraft.client.texture.NativeImage;
 import net.minecraft.client.texture.NativeImageBackedTexture;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,8 +15,6 @@ import ru.harimasa.config.HCScreen;
 import ru.harimasa.listener.OverlayReloadListener;
 
 import java.awt.*;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 @Mixin(OverlayTexture.class)
 public abstract class MixinOverlayTexture implements OverlayReloadListener {
@@ -31,25 +28,19 @@ public abstract class MixinOverlayTexture implements OverlayReloadListener {
 		OverlayReloadListener.register(this);
 	}
 
-	public void onOverlayReload() {
+	public void _1_21_5$onOverlayReload() {
 		this.reloadOverlay();
 	}
 
+	@Unique
 	private static int getColorInt(int red, int green, int blue, int alpha) {
 		alpha = 255 - alpha;
 		return (alpha << 24) + (blue << 16) + (green << 8) + red;
 	}
 
+	@Unique
 	public void reloadOverlay() {
 		NativeImage nativeImage = this.texture.getImage();
-		Class<?> clazz = NativeImage.class;
-		Method setColorNotPrivate = null;
-		try {
-			setColorNotPrivate = clazz.getDeclaredMethod("setColor", int.class, int.class, int.class);
-		} catch (NoSuchMethodException e) {
-			throw new RuntimeException(e);
-		}
-		setColorNotPrivate.setAccessible(true);
 
 		for (int i = 0; i < 16; ++i) {
 			for (int j = 0; j < 16; ++j) {
@@ -57,17 +48,9 @@ public abstract class MixinOverlayTexture implements OverlayReloadListener {
 					Color color = HCScreen.CONFIG.instance().color;
 					assert nativeImage != null;
 					if (HCScreen.CONFIG.instance().enable) {
-						try {
-							setColorNotPrivate.invoke(nativeImage, j, i, getColorInt(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()));
-						} catch (IllegalAccessException | InvocationTargetException e) {
-							throw new RuntimeException(e);
-						}
+						nativeImage.setColor(j, i, getColorInt(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()));
 					} else {
-						try {
-							setColorNotPrivate.invoke(nativeImage,j, i, -1308622593);
-						} catch (IllegalAccessException | InvocationTargetException e) {
-							throw new RuntimeException(e);
-						}
+						nativeImage.setColor(j, i, -1308622593);
 					}
 				}
 			}
